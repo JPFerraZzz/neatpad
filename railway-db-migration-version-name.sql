e5e88bfdd3c806d351f1c4fe4548eea99ffbcfb9
JPFerraZzz <jpferrazpereira@gmail.com>
2026-04-20 11:41:26 +0100

    security: remove sensitive and legacy files

--- railway-db-migration-version-name.sql
+++
@@ -1,4 +0,0 @@
--- Executar no MySQL do Railway se a tabela note_versions já existir sem a coluna version_name.
--- Permite dar um nome às versões guardadas manualmente.
-
-ALTER TABLE note_versions ADD COLUMN version_name VARCHAR(255) DEFAULT NULL AFTER saved_by;
